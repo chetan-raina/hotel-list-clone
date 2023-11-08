bd8a692d0f0ff5cc5d87cf4c364ce48ffec825b0
Chetan <chetan@Chetans-MacBook-Air.local>
2023-11-08 14:57:47 +0530

    comment store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store'
+// import store from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <App />
-  </Provider>
+  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
